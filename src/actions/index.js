4d6a7da7a10742847fbc4e58d3f461aa79375b1b
Banchee501 <rosstactical373@gmail.com>
2022-10-30 20:05:40 +0200

    debagging, use createSlice for actions and reducers

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
-import { createAction } from "@reduxjs/toolkit";
-
+import { heroesFetching, heroesFetched, heroesFetchingError } from '../components/heroesList/heroesSlice';
+import { filtersFetching, filtersFetched, filtersFetchingError } from '../components/heroesFilters/filtersSlice';
 export const fetchHeroes = (request) => (dispatch) => {
     dispatch(heroesFetching());
     request("http://localhost:3001/heroes")
@@ -7,32 +7,9 @@ export const fetchHeroes = (request) => (dispatch) => {
         .catch(() => dispatch(heroesFetchingError()))
 }
 
-export const heroesFetching = createAction('HEROES_FETCHING');
-
-export const heroesFetched = createAction('HEROES_FETCHED');
-
-export const heroesFetchingError = createAction('HEROES_FETCHING_ERROR');
-
 export const fetchFilters = (request) => (dispatch) => {
     dispatch(filtersFetching());
     request("http://localhost:3001/filters")
         .then(data => dispatch(filtersFetched(data)))
         .catch(() => dispatch(filtersFetchingError()))
 }
-
-export const filtersFetching = createAction('FILTERS_FETCHING');
-
-export const filtersFetched = createAction('FILTERS_FETCHED');
-
-export const filtersFetchingError = createAction('FILTERS_FETCHING_ERROR');
-
-export const activeFilterChanged = (filter) => (dispatch) => {
-    return {
-        type: 'ACTIVE_FILTER_CHANGED',
-        payload: filter
-    }
-}
-
-export const heroCreated = createAction('HERO_CREATED');
-
-export const heroDeleted = createAction('HERO_DELETED');
